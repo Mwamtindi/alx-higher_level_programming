683bb6ae2dea7c1ab37430558cb476c4f521307a
Mwamtindi <mwamtindishaban@gmail.com>
2023-06-07 19:18:03 +0000

    islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-def islower(c)
-for c in range(97, 123):
-    return('{}'.format(chr(c)), end='')
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
