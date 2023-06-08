17143435ab19b756a811a53cd7d09f962f4b2812
Mwamtindi <mwamtindishaban@gmail.com>
2023-06-08 20:43:21 +0000

    number and lists of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    tally = len(sys.argv) - 1
+    if tally == 0:
+        print("0 arguments.")
+    elif tally == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(tally))
+        for t in range(tally):
+            print("{}: {}".format(t + 1, sys.argv[t + 1]))
