7d69af17ab0ceec1dcdc87360237d991b7b414fd
Mwamtindi <mwamtindishaban@gmail.com>
2023-06-08 20:23:42 +0000

    function that does some maths

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, subtract, multiply, divide
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
     tsum = add(a, b)
-    tdifference = subtract(a, b)
-    tproduct = multiply(a, b)
-    tquotient = divide(a, b)
+    tdifference = sub(a, b)
+    tproduct = mul(a, b)
+    tquotient = div(a, b)
     print("{:d} + {:d} = {:d}".format(a, b, tsum))
     print("{:d} - {:d} = {:d}".format(a, b, tdifference))
     print("{:d} * {:d} = {:d}".format(a, b, tproduct))
