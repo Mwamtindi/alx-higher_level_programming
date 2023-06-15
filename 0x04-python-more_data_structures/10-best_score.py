c972aed6afcfbc4667f50e7cb1e41f15ce0e87d6
Mwamtindi <mwamtindishaban@gmail.com>
2023-06-15 15:57:21 -0400

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,10 +2,10 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    max_key = None
-    max_value = float('-inf')
+    max_kkey = None
+    max_vvalue = float('-inf')
     for key, value in a_dictionary.items():
-        if value > max_value:
-            max_key = key
-            max_value = value
-    return max_key
+        if value > max_vvalue:
+            max_kkey = key
+            max_vvalue = value
+    return max_kkey
