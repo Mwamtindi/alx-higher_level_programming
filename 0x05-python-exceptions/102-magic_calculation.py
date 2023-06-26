006be175b15cfcfb3ef796188a1c23a2e0331665
Mwamtindi <mwamtindishaban@gmail.com>
2023-06-26 16:10:11 -0400

    python fumcyion

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def magic_calculation(a, b):
+    res = 0
+
+    for w in range(1, 3):
+        try:
+            if w > a:
+                raise Exception('Too far')
+
+            res += a ** b / w
+        except:
+            res = b + a
+            break
+
+    return res
