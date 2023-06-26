df15e1b125996fe2cf4c3714a5db124671bb539c
Mwamtindi <mwamtindishaban@gmail.com>
2023-06-26 13:33:28 -0400

    prints an integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 def safe_print_integer(value):
     try:
         formatted_value = "{:d}".format(value)
-    print(formatted_value)
+        print(formatted_value)
         return True
     except (ValueError, TypeError):
         return False
-
