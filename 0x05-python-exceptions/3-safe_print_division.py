c51470d8e94a3dbf49fd0c001dcb903d55deca25
Mwamtindi <mwamtindishaban@gmail.com>
2023-06-26 14:28:06 -0400

    divides 2 integers

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        res = a / b
+        divd = a / b
     except ZeroDivisionError:
-        return None
+        divd = None
     finally:
-        print("Inside result: {}".format(res))
-    
-    return res
+        print("Inside result: {}".format(divd))
+    return divd
