4775873e355919e7d86c5b2198f1a7872b00ebbd
Mwamtindi <mwamtindishaban@gmail.com>
2024-04-12 20:26:40 +0300

    how many instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,10 +5,13 @@
 class Rectangle:
     """Class defining rectangle based on 4-rectangle.py"""
 
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Width and height initialization"""
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
