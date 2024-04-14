edbe9f17f432100fa4848813ee9a5419ef0a0ac3
Mwamtindi <mwamtindishaban@gmail.com>
2024-04-14 22:54:59 +0300

    integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,20 +4,15 @@
 
 
 def add_integer(a, b=98):
-    """Add two integers or floats and return the result as an integer.
+    """Result of integer addition of a and b.
 
-    Args:
-        a (int, float): The first number.
-        b (int, float): The second number.
-
+    Perform check to know if a and b are ints before addition
     Raises:
-        TypeError: If either `a` or `b` is not an integer or float.
-        OverflowError: If the result of the addition exceeds the maximum integer value.
-        ValueError: If either `a` or `b` is a float NaN.
-
-    Returns:
-        int: The result of the addition.
+    TypeError: if one of them is non-integer or float
+    OverflowError: if the addition result exceeds the maximum integer value
+    ValueError: if one of the values is a float NaN
     """
+
     if not (isinstance(a, int) or isinstance(a, float)):
         raise TypeError("a must be an integer or float")
     if not (isinstance(b, int) or isinstance(b, float)):
@@ -26,7 +21,6 @@ def add_integer(a, b=98):
     try:
         return int(a) + int(b)
     except OverflowError:
-        raise OverflowError("integer addition result is too large to convert to int")
+        raise OverflowError("integer addition large to convert to int")
     except ValueError:
         raise ValueError("cannot convert float NaN to integer")
-
