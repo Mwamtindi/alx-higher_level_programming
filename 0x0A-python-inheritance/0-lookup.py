24e280cd72e15d25c9e06811ce75b77617192fe9
Mwamtindi <mwamtindishaban@gmail.com>
2024-04-15 14:56:30 +0300

    lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
+"""Module that defines lookup function"""
 
 
 def lookup(obj):
     """Returns a list of attributes and methods of an object."""
     return dir(obj)
-
-# Example usage
-class Cybertawk:
-    def __init__(self):
-        self.attribute1 = 1
-
-    def method1(self):
-        pass
-
-obj = Cybertawk()
-print(lookup(obj))
