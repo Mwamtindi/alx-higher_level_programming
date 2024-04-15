55a4196fea0d2e9457217a1d7307b316302b6e7f
Mwamtindi <mwamtindishaban@gmail.com>
2024-04-15 17:35:59 +0300

    only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that defines inherits_from method."""
+
+
+def inherits_from(obj, a_class):
+    """Checks if an object is a true subclass of a class."""
+    return isinstance(obj, a_class) and type(obj) != a_class
