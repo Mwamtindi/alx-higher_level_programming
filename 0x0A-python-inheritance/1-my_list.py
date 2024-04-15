26f7d89517efd59db30f20a30e43e5ed39b22904
Mwamtindi <mwamtindishaban@gmail.com>
2024-04-15 15:44:12 +0300

    my list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-'''Module for MyList class.'''
+"""Module for MyList inherited list"""
 
 
 class MyList(list):
-    '''Custom MyList class.'''
+    """MyList class."""
     def print_sorted(self):
-        '''Method for printing sorted list.'''
+        """Method to print sorted list."""
         print(sorted(self))
