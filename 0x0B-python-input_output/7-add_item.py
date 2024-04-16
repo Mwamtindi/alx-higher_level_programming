618b6d2d735661ceeed23769be088155b8da7ef8
Mwamtindi <mwamtindishaban@gmail.com>
2024-04-16 21:09:56 +0300

    load,add,save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A module that adds all arguments to a python list and save them."""
+"""A module that adds all arguments to a python list and saves them."""
 
 
 import sys
@@ -7,8 +7,7 @@ import sys
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
     try:
         items = load_from_json_file("add_item.json")
@@ -16,4 +15,3 @@ if __name__ == "__main__":
         items = []
     items.extend(sys.argv[1:])
     save_to_json_file(items, "add_item.json")
-
