43bdd7f0fde4530a6d87ab1e46a095d51ecc83c9
Mwamtindi <mwamtindishaban@gmail.com>
2024-04-16 14:59:30 +0300

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 
 
 def read_file(filename=""):
+    """Reads a text file and prints it to stdout."""
     with open(filename, encoding="utf-8") as file:
         for line in file:
             print(line, end='')
