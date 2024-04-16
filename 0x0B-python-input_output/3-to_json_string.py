453af9b8a23bcdb38b9789e1f400fbb7ee20c157
Mwamtindi <mwamtindishaban@gmail.com>
2024-04-16 15:19:49 +0300

    to json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A module that defines to_json_string function."""
+
+
+import json
+"""Import json module."""
+
+def to_json_string(my_obj):
+    """Returns JSON rep of an object"""
+    return json.dumps(my_obj)
