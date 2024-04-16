d7863bd16771f7faf2161ca3b767adb926372653
Mwamtindi <mwamtindishaban@gmail.com>
2024-04-16 15:53:46 +0300

    load,add,save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,8 +5,8 @@
 import sys
 
 
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+from 5-save_to_json_file.py import save_to_json_file
+from 6-load_from_json_file.py import load_from_json_file
 
 try:
     items = load_from_json_file("add_item.json")
