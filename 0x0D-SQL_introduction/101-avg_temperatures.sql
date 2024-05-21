231fd59a68447465d9a754ce3d491702149fda9e
Mwamtindi <mwamtindishaban@gmail.com>
2024-05-22 00:19:34 +0300

    import in hbtn_0c_0

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,5 @@
--- import in hbtn_0c_0 db this table dump
-SELECT city, AVG('value') AS avg_temp FROM temperatures GROUP BY city
-ORDER BY avg_temp DESC;
+-- import in hbtn_0c_0 db this table dum`p
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM temperatures
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
