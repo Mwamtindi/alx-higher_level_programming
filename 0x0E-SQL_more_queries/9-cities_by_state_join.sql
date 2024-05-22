69ea6befad79397a974d60e190864a5264de2f01
Mwamtindi <mwamtindishaban@gmail.com>
2024-05-22 23:30:22 +0300

    cities by states

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,5 @@
 -- list all cities in the db hbtn_0d_usa
 -- records display: cities.id - cities.name - states.name sorted ASC.
-
 SELECT ci.id, ci.name, st.name
 FROM cities AS ci
 INNER JOIN states AS st ON ci.states_id = st.id
