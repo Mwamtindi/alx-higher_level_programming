0fc35a7bab1ddfa8a5e2f200640af777d34db15a
Mwamtindi <mwamtindishaban@gmail.com>
2024-05-23 00:03:36 +0300

    rotten tomatoes

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- script-lists all shows from hbtn_0d_tvshows_rate by their rating
+-- Each record should display: tv_shows.title - rating sum sorted DESC.
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating FROM tv_shows
+JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.id
+ORDER BY rating DESC;
