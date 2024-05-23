69948ac423bc21fd6225ae1824ceffefcf404524
Mwamtindi <mwamtindishaban@gmail.com>
2024-05-23 19:32:51 +0300

    cities of California

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,6 @@
 -- lists all the cities of California found in db hbtn_0d_usa
 -- states contain one record name=California, Result ASC no JOIN.
-USE hbtn_0d_usa;
-SELECT * FROM `cities`
-WHERE `state_id` = (
-	SELECT `id` FROM `states`
-	WHERE `name` = California
-)
-ORDER BY `id` ASC;
+SELECT `id`, `name` FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id` FROM `states` WHERE `name` = "California")
+ ORDER BY `id`;
