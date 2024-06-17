f6b59138b0c4e59c5d3ba1f12b524b217de7c5af
Mwamtindi <mwamtindishaban@gmail.com>
2024-06-17 21:28:57 +0300

    second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const args = process.argv.slice(2).map(Number);
+
+if (args.length <= 1) {
+  console.log(0);
+} else {
+  const max = Math.max(...args);
+  const filteredArgs = args.filter(num => num !== max);
+  const secondMaxi = Math.max(...filteredArgs);
+  console.log(secondMaxi);
+}
