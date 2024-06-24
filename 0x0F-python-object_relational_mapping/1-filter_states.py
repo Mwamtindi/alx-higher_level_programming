122633853fab883a85aaa2d94f5f79a9004c01a8
Mwamtindi <mwamtindishaban@gmail.com>
2024-06-24 21:25:11 +0300

    filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
          host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db_connection.cursor()
 
-    cursor.execute( "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' \
                 ORDER BY states.id ASC")
 
     all_states = cursor.fetchall()
