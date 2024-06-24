863e82094dea4d0c840ba583aead2c591e53968f
Mwamtindi <mwamtindishaban@gmail.com>
2024-06-24 21:08:11 +0300

    sql inection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+A script that takes in args and displays all values in the 'states' table
++ of 'hbtn_0e_0_usa' where 'name' matches the arg.This time script is
++ safe from MySQL injections!
+"""
+
+import MySQLdb as db
+from sys import argv
+
+if __name__ == "__main__":
+    """
+    Connects to the db, retrieves all states where the name matches the
+    provided arg, sorts them by ID and prints them.
+    """
+    db_connection = db.connect(host="localhost", port=3306,
+            user=argv[1], passwd=argv[2], db=argv[3])
+
+    cursor = db_connection.cursor()
+    cursor.execute(
+         "SELECT * FROM states WHERE name LIKE \
+                    BINARY %(name)s ORDER BY states.id ASC", {'name': argv[4]})
+
+    all_states = cursor.fetchall()
+
+    for state in all_states:
+        print(state)
