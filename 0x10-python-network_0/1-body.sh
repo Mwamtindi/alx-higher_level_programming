56b6064e0406a5be1ab518e1e6ea7351cdd82481
Mwamtindi <mwamtindishaban@gmail.com>
2024-07-04 20:55:53 +0300

    curl to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that takes in URL, sends a GET request to URL, and displays the body
-[ "$(curl -s -o /dev/null -w '%{http_code}' "$1")" -eq 200 ] && curl -s "$1"
+if [ "$(curl -s -o /dev/null -w '%{http_code}' "$1")" -eq 200 ]; then curl -s "$1"; fi
