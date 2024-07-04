d2b37fe517e737b385b7e2fd9150cfb0c446a8f7
Mwamtindi <mwamtindishaban@gmail.com>
2024-07-04 22:31:04 +0300

    you got me!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes req to '0.0.0.0:5000/catch_me' causes the server respond with message
-curl -s -X PUT 0.0.0.0:5000/catch_me
+curl -s -X PUT -L -d "user_id=98" "0.0.0.0:5000/catch_me"
