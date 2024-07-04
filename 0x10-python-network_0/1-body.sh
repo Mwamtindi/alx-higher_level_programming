7665ab0ab63fbc6e35d6e8d4f151bf5c3935fa0b
Mwamtindi <mwamtindishaban@gmail.com>
2024-07-04 22:37:19 +0300

    curl to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that takes in URL, sends a GET request to URL, and displays the body
-curl -s -o /dev/stderr -w "%{http_code}" "$1"
+curl -s -w "%{http_code}\n" "$1" | grep -q "200" && curl -s "$1"
