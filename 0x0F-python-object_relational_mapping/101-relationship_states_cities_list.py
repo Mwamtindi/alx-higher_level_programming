f81f06f8cda6a0e410146bc8af37f0216787e28d
Mwamtindi <mwamtindishaban@gmail.com>
2024-07-09 00:18:28 +0300

    list relationship

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """
-A script that lists all State objects, and corresponding City objects,
-cointained in the database hbtn_0e_101_usa
+This module lists all State objects and their corresponding City objects
+from the database hbtn_0e_101_usa using SQLAlchemy ORM.
+
+It demonstrates the use of SQLAlchemy to query a MySQL database and
+display the relationships between States and Cities.
 """
+
 import sys
 from relationship_state import Base, State
 from relationship_city import City
@@ -12,25 +16,26 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """
-    Connect to the database, creates database tables if they don't exist,
-    initializes a SQLAlchemy session for db interactions, queries the db
-    for all State objects, outer joining with City objects and Prints each
-    State and its associated Cities in a formatted output.
-    """
+    Main execution of the script.
 
-    database_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'. format(
+    Connects to the database, creates tables if they don't exist,
+    initializes a SQLAlchemy session for database interactions,
+    queries the database for all State objsouter joining with City objects
+    and prints each State and its associated Cities in a formatted output.
+    """
+    database_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3])
     db_engine = create_engine(database_url, pool_pre_ping=True)
-
     Base.metadata.create_all(db_engine)
-
+    
     SessionMaker = sessionmaker(bind=db_engine)
     db_session = SessionMaker()
-
-    states_query = db_session.query(State).outerjoin(City).
-    order_by(State.id, City.id).all()
-
+    
+    states_query = db_session.query(State).outerjoin(City).order_by(State.id, City.id).all()
+    
     for state in states_query:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
             print("    {}: {}".format(city.id, city.name))
+
+    db_session.close()
