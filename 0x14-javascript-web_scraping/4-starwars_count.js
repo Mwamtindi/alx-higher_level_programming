911186cad8d52dada783ef3ec454bec44250b003
Mwamtindi <mwamtindishaban@gmail.com>
2024-07-30 22:55:54 +0300

    star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,11 +4,12 @@ const request = require('request');
 
 // Get the URL and file path from the command line arguments
 const apiUrl = process.argv[2];
+
 const characterId = 18;
 
-request(apiUrl, (errr, response, body) => {
-  if (errr) {
-    console.error(errr);
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
   } else {
     const filmsData = JSON.parse(body).results;
     const moviesWithWedge = filmsData.filter((film) =>
