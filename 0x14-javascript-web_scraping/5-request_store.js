f91381556b55847812366ba08f710d416256b768
Mwamtindi <mwamtindishaban@gmail.com>
2024-07-30 22:37:13 +0300

    Loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -7,21 +7,13 @@ const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-if (!url || !filePath) {
-  console.error('Error: Both URL and file path must be provided.');
-  process.exit(1);
-}
-
-// Perform a GET request to the URL
-request.get(url, (error, response, body) => {
+request(url, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    fs.writeFile(filePath, body, 'utf8', (writeError) => {
-      if (writeError) {
-        console.error(writeError);
-      } else {
-        console.log('File writen successful.');
+    fs.writeFile(filePath, body, 'utf-8', (errr) => {
+      if (errr) {
+        console.error(errr);
       }
     });
   }
