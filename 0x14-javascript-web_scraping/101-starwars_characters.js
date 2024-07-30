68511e588a35f8d5db22b9a74512ca4ef1ec3e13
Mwamtindi <mwamtindishaban@gmail.com>
2024-07-30 22:45:16 +0300

    right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,25 +3,25 @@
 const request = require('request');
 
 // Get the API URL from the command line arguments
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
 request(apiUrl, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    const charactersUrls = JSON.parse(body).characters;
+  if (!error) {
+    const characters = JSON.parse(body).characters;
 
-    const fetchCharacterName = (characterUrl) => {
-      request(characterUrl, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-          const characterData = JSON.parse(body);
-          console.log(characterData.name);
-        } else {
-          console.error(`Error: Failed to retrieve the character data. Status code: ${response.statusCode}`);
-        }
-      });
-    };
-
-    charactersUrls.forEach(fetchCharacterName);
-  } else {
-    console.error(`Error: Failed to retrieve the movie data. Status code: ${response.statusCode}`);
+    printCharacters(characters, 0);
   }
 });
+
+// A function to print characters recursively
+function printCharacters (characters, idx) {
+  request(characters[idx], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+
+      if (idx + 1 < characters.length) {
+        printCharacters(characters, idx + 1);
+      }
+    }
+  });
+}
