8fa418acdc94ed4fbd70d7cc26dad8bfbc3f3b20
Mwamtindi <mwamtindishaban@gmail.com>
2024-07-30 22:51:56 +0300

    star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,26 +4,18 @@ const request = require('request');
 
 // Get the URL and file path from the command line arguments
 const apiUrl = process.argv[2];
-
-if (!apiUrl) {
-  console.error('Error: The API URL not provided.');
-  process.exit(1);
-}
-
 const characterId = 18;
 
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
+request(apiUrl, (errr, response, body) => {
+  if (errr) {
+    console.error(errr);
   } else {
-    if (response.statusCode === 200) {
-      const filmsData = JSON.parse(body);
-      const moviesWithCharacter = filmsData.results.filter((film) =>
-        film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-      );
-      console.log(moviesWithCharacter.length);
-    } else {
-      console.error(`Error: Failed to retrieve the movies data. Status code: ${response.statusCode}`);
-    }
+    const filmsData = JSON.parse(body).results;
+    const moviesWithWedge = filmsData.filter((film) =>
+      film.characters.includes(
+        `https://swapi-api.alx-tools.com/api/people/${characterId}/`
+      )
+    );
+    console.log(moviesWithWedge.length);
   }
 });
