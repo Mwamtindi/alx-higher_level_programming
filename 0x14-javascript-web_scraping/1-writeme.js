a84e8202590191463cae6bba0dcfe9fc46dca1ab
Mwamtindi <mwamtindishaban@gmail.com>
2024-07-30 22:33:55 +0300

    write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,16 +5,8 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-if (!filePath || !content) {
-  console.error('Error: Both the file path and content should be given');
-  process.exit(1);
-}
-
-// Write the string to the file
-fs.writeFile(filePath, content, 'utf-8', (error) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log('File written successful');
+fs.writeFile(filePath, content, 'utf-8', (errr) => {
+  if (errr) {
+    console.error(errr);
   }
 });
