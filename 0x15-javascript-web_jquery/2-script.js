ddb1566124d84da5100af95232ac1ddbd5b2c082
Mwamtindi <mwamtindishaban@gmail.com>
2024-08-07 23:22:45 +0300

    click and turn red

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,4 +1,4 @@
-'''JS script that updates the text color of the <header> element to red'''
+// JS script that updates the text color of the <header> element to red
 $("div#red_header").click(function () {
   $("header").css("color", "red");
 });
