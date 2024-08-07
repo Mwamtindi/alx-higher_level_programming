8475fcbe7f7e4b5a9e4330ff37ee17baf57361a6
Mwamtindi <mwamtindishaban@gmail.com>
2024-08-07 23:24:55 +0300

    no jquery

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,3 +1,3 @@
-'''JS script that updates the text color of the <header> element tored'''
+// JS script that updates the text color of the <header> element to red
 const header = document.querySelector("header");
 header.style.color = "#FF0000";
